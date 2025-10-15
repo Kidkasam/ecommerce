62d391b12ce1242a3bcb15ef80d0564ff4266ca7
kidus <kidus6135@gmail.com>
2025-10-15 17:33:08 +0300

    feat: add contact page

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='container'>
+      <form action="">
+        <input type="text" />
+        <input type="email" />
+        
+      </form>
+    </div>
+  )
+}
+
+export default Contact
