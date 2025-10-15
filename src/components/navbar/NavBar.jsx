5c62c9b14a7adaa0108e7718c108e2703169c5a6
kidus <kidus6135@gmail.com>
2025-10-15 17:32:40 +0300

    feat: add navbar component

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa"; 
+import "./NavBar.css";
+
+const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+ 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="container">
+      <div className="logo">ecommerce</div>
+
+
+      <div className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/about">About Us</Link>
+        <Link to="/cart">Cart</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+
+      <div className="burger-icon" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <div className={`nav mobile-nav ${menuOpen ? "open" : ""}`}>
+        <Link to="/" onClick={toggleMenu}>
+          Home
+        </Link>
+        <Link to="/products" onClick={toggleMenu}>
+          Products
+        </Link>
+        <Link to="/about" onClick={toggleMenu}>
+          About Us
+        </Link>
+        <Link to="/cart" onClick={toggleMenu}>
+          Cart
+        </Link>
+        <Link to="/contact" onClick={toggleMenu}>
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
