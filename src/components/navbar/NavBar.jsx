b6803217b4636a2d02d68574aa63681d84104cf0
kidus <kidus6135@gmail.com>
2025-10-15 18:38:26 +0300

    updated the navbar component

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -12,40 +12,30 @@ const NavBar = () => {
   };
 
   return (
-    <div className="container">
-      <div className="logo">ecommerce</div>
+<div className="navbar">
+  <div className="logo">ecommerce</div>
 
+  <div className="nav">
+    <Link to="/">Home</Link>
+    <Link to="/products">Products</Link>
+    <Link to="/about">About Us</Link>
+    <Link to="/cart">Cart</Link>
+    <Link to="/contact">Contact</Link>
+  </div>
 
-      <div className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/products">Products</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/cart">Cart</Link>
-        <Link to="/contact">Contact</Link>
-      </div>
+  <div className="burger-icon" onClick={toggleMenu}>
+    {menuOpen ? <FaTimes /> : <FaBars />}
+  </div>
 
-      <div className="burger-icon" onClick={toggleMenu}>
-        {menuOpen ? <FaTimes /> : <FaBars />}
-      </div>
+  <div className={`mobile-nav ${menuOpen ? "open" : ""}`}>
+    <Link to="/" onClick={toggleMenu}>Home</Link>
+    <Link to="/products" onClick={toggleMenu}>Products</Link>
+    <Link to="/about" onClick={toggleMenu}>About Us</Link>
+    <Link to="/cart" onClick={toggleMenu}>Cart</Link>
+    <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+  </div>
+</div>
 
-      <div className={`nav mobile-nav ${menuOpen ? "open" : ""}`}>
-        <Link to="/" onClick={toggleMenu}>
-          Home
-        </Link>
-        <Link to="/products" onClick={toggleMenu}>
-          Products
-        </Link>
-        <Link to="/about" onClick={toggleMenu}>
-          About Us
-        </Link>
-        <Link to="/cart" onClick={toggleMenu}>
-          Cart
-        </Link>
-        <Link to="/contact" onClick={toggleMenu}>
-          Contact
-        </Link>
-      </div>
-    </div>
   );
 };
 
