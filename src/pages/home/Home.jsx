b33130ad5e85497d696a1406d9ab33f0c9f0de89
kidus <kidus6135@gmail.com>
2025-10-20 14:35:50 +0300

    updated the hero section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,44 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import Hero from '../../assets/img/hero4.png';
+import Hero1 from '../../assets/img/hero4.png';
 import Banner from '../../assets/img/banner/b1.jpg';
-import './Home.css'; // Make sure CSS is in this file
+import Banner1 from '../../assets/img/blog/b2.jpg';
+import Banner2 from '../../assets/img/banner/b17.jpg';
+
+import './Home.css';
+
+const slides = [
+  {
+    title: "70% Off!",
+    subtitle: "Grab your favorite products now",
+    img: Hero1,
+    btnText: "Shop Now",
+  },
+  {
+    title: "50% Off!",
+    subtitle: "Limited time offer",
+    img: Banner2,
+    btnText: "Explore",
+  },
+  {
+    title: "30% Off!",
+    subtitle: "Don't miss out",
+    img: Banner1,
+    btnText: "Buy Now",
+  },
+];
 
 const Home = () => {
-  const slides = [
-    { title: "70% off", img: Hero },
-    { title: "50% off", img: Hero },
-    { title: "30% off", img: Hero }
-  ];
-
   const [index, setIndex] = useState(0);
 
-  const nextSlide = () => setIndex(prev => (prev + 1) % slides.length);
-  const prevSlide = () => setIndex(prev => (prev - 1 + slides.length) % slides.length);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex(prev => (prev + 1) % slides.length);
+    }, 5000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const nextSlide = () => setIndex((prev) => (prev + 1) % slides.length);
+  const prevSlide = () => setIndex((prev) => (prev - 1 + slides.length) % slides.length);
 
   return (
-    <>
-      <div className='hero'>
+    <div>
+      
+      <section className="hero">
         <img src={slides[index].img} alt={slides[index].title} />
         <div className="overlay">
-          <h2 className="hero-title">{slides[index].title}</h2>
+          <h1>{slides[index].title}</h1>
+          <p>{slides[index].subtitle}</p>
+          <button className="cta-btn">{slides[index].btnText}</button>
         </div>
-        <div className="btn">
-          <button className='prev' onClick={prevSlide}>
-            <FaChevronLeft />
-          </button>
-          <button className='next' onClick={nextSlide}>
-            <FaChevronRight />
-          </button>
-        </div>
-      </div>
+        <button className="arrow prev" onClick={prevSlide}>
+          <FaChevronLeft />
+        </button>
+        <button className="arrow next" onClick={nextSlide}>
+          <FaChevronRight />
+        </button>
+      </section>
 
-      <div className="banner">
+      
+      <section className="banners">
         <img src={Banner} alt="Banner" />
-    
-      </div>
-    </>
+      </section>
+    </div>
   );
-}
+};
 
 export default Home;
