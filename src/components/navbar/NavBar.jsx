5d72e21ba2d58f90ac9ddb591b0988782c619f4e
kidus <kidus6135@gmail.com>
2025-10-24 18:47:04 +0300

    feat: updated navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -13,12 +13,11 @@ const NavBar = () => {
 
   return (
 <div className="navbar">
-  <div className="logo">ecommerce</div>
+  <div className="logo">KFITS</div>
 
   <div className="nav">
     <Link to="/">Home</Link>
     <Link to="/products">Products</Link>
-    <Link to="/about">About Us</Link>
     <Link to="/cart">Cart</Link>
     <Link to="/contact">Contact</Link>
   </div>
@@ -30,7 +29,7 @@ const NavBar = () => {
   <div className={`mobile-nav ${menuOpen ? "open" : ""}`}>
     <Link to="/" onClick={toggleMenu}>Home</Link>
     <Link to="/products" onClick={toggleMenu}>Products</Link>
-    <Link to="/about" onClick={toggleMenu}>About Us</Link>
+   
     <Link to="/cart" onClick={toggleMenu}>Cart</Link>
     <Link to="/contact" onClick={toggleMenu}>Contact</Link>
   </div>
