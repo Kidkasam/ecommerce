d2f43d5729e9ff69cbf6b863f7221bc6772f8ccf
kidus <Kidkasam@users.noreply.github.com>
2026-03-04 11:59:27 +0300

    updated serializer.py

--- ecommerce_backend/users/serializers.py
+++ ecommerce_backend/users/serializers.py
@@ -1,9 +1,10 @@
-from djangorestframework import serializers
+from rest_framework import serializers
 from .models import User
 
-class UserSerializer(serializers):
-    model = User
-    fields = ['phone_number', 'email', 'first_name', 'last_name',]
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['phone_number', 'email', 'first_name', 'last_name',]
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=100, write_only=True)
@@ -14,11 +15,11 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['phone_number', 'email', 'first_name', 'last_name', 'password', 'password_confirm']
 
         def validate(self, data):
-            if data['password'] != ['password_confirm']:
+            if data['password'] != data['password_confirm']:
 
-                raise serializers.validationError({"password": "passwords do not match"})
+                raise serializers.ValidationError({"password": "passwords do not match"})
             return data
         def Create(self, validated_data):
             validated_data.pop('password_confirm')
-            user= User.object.create(**validated_data)
+            user= User.object.create_user(**validated_data)
             return user
