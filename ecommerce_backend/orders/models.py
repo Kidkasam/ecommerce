7046dc4b003a6d9a8842819590cb3e660f20aee6
kidus <Kidkasam@users.noreply.github.com>
2026-04-03 18:02:02 +0300

    feat: complete cart and order  app with rest framework views

--- ecommerce_backend/orders/models.py
+++ ecommerce_backend/orders/models.py
@@ -23,6 +23,8 @@ class Order(models.Model):
     user  = models.ForeignKey(User, on_delete = models.CASCADE)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    stripe_payment_id = models.CharField(max_length=255, blank=True, null=True)
+    stripe_payment_status = models.CharField(max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
